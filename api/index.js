ed49a6b20694e0694d5176c4c58507a37d704e6e
vishnu-madhav-2454 <vishnumadhav2454@gmail.com>
2025-08-09 01:41:52 +0530

    a lot of changes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongooose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -10,9 +12,16 @@ mongooose.connect(process.env.MONGO).then(() => {
     console.error('Error connecting to MongoDB:', error);
 });
 
+
+
 const app = express();
 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use(express.json());
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
